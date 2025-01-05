f709f82aec8ee850eecb0a57545e4260c9d447f1
wenhu1024 <1340773297@qq.com>
2025-01-05 22:04:50 +0800

    [ch5][your_first_pipeline] write pipeline

--- ch5/your_first_pipeline/your_turn/passPipelineWithLegacyPM.cpp
+++ ch5/your_first_pipeline/your_turn/passPipelineWithLegacyPM.cpp
@@ -1,5 +1,17 @@
+#include "llvm/IR/LLVMContext.h"
 #include "llvm/IR/LegacyPassManager.h" // For legacy::PassManager.
+#include "llvm/Transforms/IPO/AlwaysInliner.h"       // For inliner pass.
+#include "llvm/Transforms/InstCombine/InstCombine.h" // For instcombine pass.
+#include "llvm/Transforms/Utils.h"                   // For mem2reg pass.
 
 using namespace llvm;
 
-void runYourTurnPassPipelineForLegacyPM(Module &MyModule) {}
+void runYourTurnPassPipelineForLegacyPM(Module &MyModule) {
+    legacy::PassManager LegacyPM;
+
+    LegacyPM.add(createPromoteMemoryToRegisterPass());
+    LegacyPM.add(createInstructionCombiningPass());
+    LegacyPM.add(createAlwaysInlinerLegacyPass());
+
+    LegacyPM.run(MyModule);
+}
--- ch5/your_first_pipeline/your_turn/passPipelineWithNewPM.cpp
+++ ch5/your_first_pipeline/your_turn/passPipelineWithNewPM.cpp
@@ -1,6 +1,55 @@
+#include "llvm/CodeGen/RegisterBank.h"
+#include "llvm/IR/LLVMContext.h"
+#include "llvm/IR/LegacyPassManager.h"
 #include "llvm/IR/Module.h"
+#include "llvm/IR/PassInstrumentation.h"
 #include "llvm/IR/PassManager.h" // For the new PassManager.
+#include "llvm/Pass.h"
+#include "llvm/Passes/PassBuilder.h"
+#include "llvm/Passes/StandardInstrumentations.h"
+#include "llvm/Transforms/IPO/AlwaysInliner.h"
+#include "llvm/Transforms/InstCombine/InstCombine.h"
+#include "llvm/Transforms/Utils.h"
+#include "llvm/Transforms/Utils/Mem2Reg.h"
 
 using namespace llvm;
 
-void runYourTurnPassPipelineForNewPM(Module &MyModule) {}
+void runYourTurnPassPipelineForNewPM(Module &MyModule) {
+    LLVMContext &Context = MyModule.getContext();
+
+    FunctionAnalysisManager FAM;
+    ModuleAnalysisManager MAM;
+
+    PassInstrumentationCallbacks PIC;
+    PrintPassOptions PrintPassOpts;
+    PrintPassOpts.Verbose=true;
+    PrintPassOpts.SkipAnalyses=false;
+    PrintPassOpts.Indent=true;
+
+    StandardInstrumentations SI(Context,true,false,PrintPassOpts);
+    SI.registerCallbacks(PIC,&MAM);
+
+    MAM.registerPass([&]{return PassInstrumentationAnalysis(&PIC);});
+    FAM.registerPass([&]{return PassInstrumentationAnalysis(&PIC);});
+
+    MAM.registerPass([&]{return FunctionAnalysisManagerModuleProxy(FAM);});
+    FAM.registerPass([&]{return ModuleAnalysisManagerFunctionProxy(MAM);});
+
+    ModulePassManager NewPM;
+    FunctionPassManager FPMgr;
+
+    PassBuilder PB;
+    PB.registerFunctionAnalyses(FAM);
+    PB.registerModuleAnalyses(MAM);
+
+#ifndef USE_MODULE_MGR
+    FPMgr.addPass(PromotePass());
+    FPMgr.addPass(InstCombinePass());
+#else   
+    NewPM.addPass(createModuleToFunctionPassAdaptor(PromotePass()));
+    NewPM.addPass(createModuleToFunctionPassAdaptor(InstCombinePass()));
+#endif
+    NewPM.addPass(createModuleToFunctionPassAdaptor(std::move(FPMgr)));
+    NewPM.addPass(AlwaysInlinerPass());
+    NewPM.run(MyModule,MAM);
+}
